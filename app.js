69fab95a1bce5be8fe1f3fcc69807bbf30803411
Santiago Marín <esmarina@unal.edu.co>
2020-04-20 17:04:02 -0500

    Cookies

--- app.js
+++ app.js
@@ -26,30 +26,42 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.use(cookieParser('12345-67890-09876-54321')); //Clave con la que se cifra o firma la cookie
 
 function auth(req, res, next) {
-  console.log(req.headers);
+  
+  if (!req.signedCookies.user) {    
+      var authHeader = req.headers.authorization;
+      if (!authHeader) {
+        var err = new Error('You aren\'t autheticated!');
+        res.setHeader('WWWW-Authenticate', 'Basic');
+        err.status = 401;   //Unauthorized
+        return next(err);
+      }
+    
+      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString()
+      var user = auth.split(':')[0];
+      var password = auth.split(':')[1];
+    
+      if (user == 'admin' && password == 'password') {
+        res.cookie('user', 'admin', {signed: true});
+        next(); //authorized
+      } else {
+        var err = new Error('You aren\'t autheticated!');
+        res.setHeader('WWWW-Authenticate', 'Basic');
+        err.status = 401;   //Unauthorized
+        next(err);
+      }
 
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error('You aren\'t autheticated!');
-    res.setHeader('WWWW-Authenticate', 'Basic');
-    err.status = 401;   //Unauthorized
-    return next(err);
-  }
-
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString()
-  var user = auth.split(':')[0];
-  var password = auth.split(':')[1];
-
-  if (user == 'admin' && password == 'password') {
-    next();
   } else {
-    var err = new Error('You aren\'t autheticated!');
-    res.setHeader('WWWW-Authenticate', 'Basic');
-    err.status = 401;   //Unauthorized
-    next(err);
+    if (req.signedCookies.user == 'admin') {
+      next();
+    } else {
+      var err = new Error('You aren\'t autheticated!');
+        err.status = 401;   //Unauthorized
+        return next(err);
+    }
   }
 }
 
