437184a3a57331e63c5dc408871c178ccc5b5ee0
Santiago Marín <esmarina@unal.edu.co>
2020-07-17 17:02:11 -0500

    CORS whitelist updated 2

--- routes/cors.js
+++ routes/cors.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 
-var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200', 'https://www.facebook.com, https://resto-confusion.herokuapp.com'];
+var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200', 'https://www.facebook.com', 'https://resto-confusion.herokuapp.com'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('origin'));
